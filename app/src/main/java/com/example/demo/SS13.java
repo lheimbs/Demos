c3625e014f4a45041b8f44e42dbed82db7754886
lheimbs <l.heimbs@gmail.com>
2019-09-29 11:10:15 +0200

    Add SS13 Classes

---
+++ app/src/main/java/com/example/demo/SS13.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.heimbsle69869.demo.R;
+
+public class SS13 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ss13);
+        setTitle(this.getClass().getSimpleName());
+    }
+}
---
+++ app/src/main/java/com/example/demo/SS13_Sideactivity.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.heimbsle69869.demo.R;
+
+public class SS13_Sideactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ss13_side);
+        setTitle(this.getClass().getSimpleName());
+    }
+}
